022f8832c29e98e86d2038a233a338842b140d58
gracken <nwngracken@gmail.com>
2018-09-29 07:33:58 -0500

    Cleaned legacy comments from PartyEvents

--- Plugins/Events/Events/PartyEvents.cpp
+++ Plugins/Events/Events/PartyEvents.cpp
@@ -5,7 +5,6 @@
 #include "Events.hpp"
 #include "Utils.hpp"
 #include "ViewPtr.hpp"
-//#include "API/Vector.hpp"
 
 namespace Events {
 
@@ -16,8 +15,6 @@ using namespace NWNXLib::Services;
 
 PartyEvents::PartyEvents(ViewPtr<Services::HooksProxy> hooker)
 {
-    //hooker->RequestSharedHook<
-    //hooker->RequestExclusiveHook<
     hooker->RequestExclusiveHook<
         NWNXLib::API::Functions::CNWSFaction__InviteMember,
         int32_t,
@@ -27,28 +24,16 @@ PartyEvents::PartyEvents(ViewPtr<Services::HooksProxy> hooker)
     >(PartyEvents::InviteMemberHook);
 }
 
-// For RequestSharedHook
-//void PartyEvents::InviteMemberHook
-// For RequestExclusiveHook
-//int32_t PartyEvents::InviteMemberHook
 int32_t PartyEvents::InviteMemberHook
 (
-    //Services::Hooks::CallType type,
     NWNXLib::API::CNWSFaction* thisPtr,
     NWNXLib::API::Types::ObjectID oidTarget,
     NWNXLib::API::Types::ObjectID oidSender
 )
 {
-    // For RequestSharedHook
-    //const bool before = type == Services::Hooks::CallType::BEFORE_ORIGINAL;
     Events::PushEventData("TARGET_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidTarget));
     Events::PushEventData("SENDER_OBJECT_ID", NWNXLib::Utils::ObjectIDToString(oidSender));
 
-    // For RequestSharedHook
-    //Events::SignalEvent(before ? "NWNX_ON_INVITE_MEMBER_BEFORE" : "NWNX_ON_INVITE_MEMBER_AFTER", thisPtr->m_nFactionId);
-    // For RequestExclusive Hook
-    //Events::SignalEvent("NWNX_ON_INVITE_MEMBER", thisPtr->m_nFactionId);
-    //return 0;
     Events::SignalEvent("NWNX_ON_INVITE_MEMBER", thisPtr->m_nFactionId);
     return 0;
 }
--- Plugins/Events/Events/PartyEvents.hpp
+++ Plugins/Events/Events/PartyEvents.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
 #include "API/Types.hpp"
-//#include "API/Vector.hpp"
 #include "API/CNWSFaction.hpp"
-//#include "Common.hpp"
 #include "Services/Hooks/Hooks.hpp"
 #include "ViewPtr.hpp"
 
@@ -15,14 +13,8 @@ public:
     PartyEvents(NWNXLib::ViewPtr<NWNXLib::Services::HooksProxy> hooker);
 
 private:
-        // For RequestSharedHook
-        //static void InviteMemberHook
-        // For ExclusiveHook
-        //static int32_t InviteMemberHook
         static int32_t InviteMemberHook
         (
-            // For RequestSharedHook
-            //NWNXLib::Services::Hooks::CallType type, 
             NWNXLib::API::CNWSFaction* thisPtr, 
             NWNXLib::API::Types::ObjectID oidTarget, 
             NWNXLib::API::Types::ObjectID oidSender
